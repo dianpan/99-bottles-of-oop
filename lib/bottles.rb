dc9e595b6b2d7b3b051227f8c4cd5cdd03af8951
Dian Pan <dian.di.pan@gmail.com>
2017-12-13 14:19:52 -0800

    Add chapter one notes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,39 +2,51 @@ class Bottles
 
   def verse(num)
     if num == 1
-      one_bottle
+      one_bottle_verse
     elsif num == 0
-      zero_bottles
+      zero_bottle_verse
     else
-      default(num)
+      default_verse(num)
     end
   end
 
   def verses(max, min)
-    max.downto(min).map { |num| verse(num) }.join("\n")
+    max.downto(min).map { |num| verse(num) }.join('\n')
   end
 
   def song
-    verses(99, 0)
+    verses(starting_bottle_count, ending_bottle_count)
   end
 
   private
 
-  def default(num)
+  def starting_bottle_count
+    99
+  end
+
+  def ending_bottle_count
+    0
+  end
+
+  def default_verse(num)
     <<~HEREDOC
       #{num} bottles of beer on the wall, #{num} bottles of beer.
-      Take one down and pass it around, #{num-1} #{num == 2 ? 'bottle' : 'bottles'} of beer on the wall.
+      Take one down and pass it around, #{num-1} #{bottle_or_bottles} of beer on the wall.
     HEREDOC
   end
 
-  def one_bottle
+  def bottle_or_bottles
+    num == 2 ? 'bottle' : 'bottles'
+  end
+
+  def one_bottle_verse
     <<~HEREDOC
       1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.
     HEREDOC
   end
 
-  def zero_bottles
+  def zero_bottle_verse
     <<~HEREDOC
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
